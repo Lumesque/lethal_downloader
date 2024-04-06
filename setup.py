8b7e0923c0ade519572defc6ee669925832d467e
Joshua Luckie <luckiejoshu@gmail.com>
2024-04-05 20:55:28 -0400

    Finishing up a setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='hello-world',
+    name='lethal_md',
     version='1.0.0',
     entry_points={
+        'console_scripts': ['lethal_md=downloader.cli.lethal_md:main'],
     }
 )
