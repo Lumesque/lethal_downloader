011186d25697706907905238b59fcf405eb6b66e
Joshua Luckie <luckiejoshu@gmail.com>
2024-04-06 13:47:53 -0400

    Fixing version control from git tag

--- lethal_md/__init__.py
+++ lethal_md/__init__.py
@@ -1,5 +1,5 @@
 import importlib
 try: 
-    __version__ = importlib.import_module('_version').__version__
+    __version__ = importlib.import_module('._version', package=__package__).__version__
 except: 
     __version__ = 'unknown'
